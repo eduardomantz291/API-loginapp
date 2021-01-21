fb67edebd2279bd2e50da2d6d05602f30caa1312
eduardomantz291 <eduardomantz2112@gmail.com>
2021-01-21 20:27:40 -0300

    express instalado e configutado!

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use('/api', routes);
+
+app.listen(3333, () => {
+  console.log("[server] -> starting!");
+  console.log("[server] -> Server started at http://localhost:3333/api");
+  
+})
