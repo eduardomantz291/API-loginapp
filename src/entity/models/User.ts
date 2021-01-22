6074a33f0661ca3cb56b4751888cefd29a6a42a1
eduardomantz291 <eduardomantz2112@gmail.com>
2021-01-22 18:46:44 -0300

    hash da senha do usuario feito

--- src/entity/models/User.ts
+++ src/entity/models/User.ts
@@ -1,4 +1,6 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn, BeforeInsert, BeforeUpdate } from 'typeorm';
+import bcrypt from 'bcryptjs';
+
 
 @Entity('users')
 class User {
@@ -13,6 +15,12 @@ class User {
 
   @Column()
   password: string;
+
+  @BeforeInsert()
+  @BeforeUpdate()
+  hashPassword() {
+    this.password = bcrypt.hashSync(this.password, 8);
+  }
 }
 
 export default User;
