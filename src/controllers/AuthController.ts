f7189dd87334d67195f78c4ebf29de529f84ee65
eduardomantz291 <eduardomantz2112@gmail.com>
2021-01-22 19:10:59 -0300

    autenticação com token feita

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+import User from '../entity/models/User';
+
+class AuthController {
+  async authenticate(req: Request, res: Response) {
+    const UserRepository = getRepository(User);
+    const { email, password } = req.body;    
+
+    const user = await UserRepository.findOne({ where: { email } });
+
+    if (!user) {
+      return res.sendStatus(401);
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      return res.sendStatus(401);
+    }
+
+    const token = jwt.sign({ id: user.id }, 'secret', { expiresIn: '1d' });
+
+    delete user.password;
+
+    return res.json({user, token});
+  } 
+}
+
+export default new AuthController();
--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,9 @@ import { Router, Request, Response } from 'express';
 const routes = Router();
 
 import UserController from './controllers/UserController';
+import AuthController from './controllers/AuthController';
 
 routes.post("/users", UserController.store);
+routes.post("/auth", AuthController.authenticate);
 
 export default routes;
