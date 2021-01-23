00e7484022967d3cd61bc29225fc6bfbdd89c735
eduardomantz291 <eduardomantz2112@gmail.com>
2021-01-22 22:14:31 -0300

    middleware de autenticação feita

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -4,6 +4,10 @@ import { getRepository } from 'typeorm';
 import User from '../entity/models/User';
 
 class UserController {
+  async index(req: Request, res: Response) {
+    return res.send({ userID: req.userId });
+  }
+
   async store(req: Request, res: Response) {
     const UserRepository = getRepository(User);
     const { name, email, password } = req.body;
--- src/routes.ts
+++ src/routes.ts
@@ -2,10 +2,13 @@ import { Router, Request, Response } from 'express';
 
 const routes = Router();
 
+import authMiddleware from './middlewares/authMiddlewares';
+
 import UserController from './controllers/UserController';
 import AuthController from './controllers/AuthController';
 
 routes.post("/users", UserController.store);
 routes.post("/auth", AuthController.authenticate);
+routes.get('/users', authMiddleware, UserController.index);
 
 export default routes;
